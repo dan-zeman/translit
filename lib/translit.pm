d3b75a5e7efc8ade261bbf7cddc6d22b51677ee9
Dan Zeman <zeman@ufal.mff.cuni.cz>
2022-07-28 10:55:05 +0200

    Translated comments in translit.pm to English.

--- lib/translit.pm
+++ lib/translit.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
-# Funkce pro přípravu transliterace do latinky.
-# (c) 2008 Dan Zeman <zeman@ufal.mff.cuni.cz>
-# Licence: GNU GPL
+# Functions to prepare and use transliteration tables.
+# Copyright © 2008, 2022 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# License: GNU GPL
 
 package translit;
 use utf8;
@@ -9,12 +9,12 @@ use utf8;
 
 
 #------------------------------------------------------------------------------
-# Ladění: vypsat převodní tabulku.
+# Debugging: print the transliteration table.
 #------------------------------------------------------------------------------
 sub vypsat
 {
-    my $prevod = shift; # odkaz na hash s převodní tabulkou
-    binmode(STDOUT, ":utf8");
+    my $prevod = shift; # reference to the hash with the transliteration table
+    binmode(STDOUT, ':utf8');
     foreach my $klic (sort(keys(%{$prevod})))
     {
         print("$klic\t$prevod->{$klic}\n");
@@ -24,21 +24,22 @@ sub vypsat
 
 
 #------------------------------------------------------------------------------
-# Převede řetězec z jednoho písma nebo kódování do druhého. Potřebujeme
-# nejdřív v příslušném modulu inicalizovat převodní tabulku (hash). Tato funkce
-# neklade omezení na délku řetězce, jehož převod může být v hashi definován,
-# ale ani hash neprochází, aby zjistila délku nejdelšího takového řetězce
-# (nebylo by to efektivní, pokud by funkce byla volána např. pro každé slovo
-# zvlášť, což třeba u anotovaného korpusu potřebujeme). Místo toho je možné jí
-# předem zjištěnou maximální délku předat jako parametr. Pokud tento parametr
-# nedostane, funkce testuje převody řetězců do délky 3 znaky.
+# Converts a string from one script or encoding to another. Before calling this
+# function, we have to initialize the transliteration table (hash) in the
+# respective module. This function does not restrict the length of the substring
+# whose transliteration can be defined in the hash, but it does not scan the
+# hash to figure out the maximal length (it would not be efficient; this
+# function may be called separately for each word, million times in a row).
+# Instead, one may to figure out the maximal length beforehand and give it to
+# the function as a parameter. Without the parameter, the function will use a
+# default value.
 #------------------------------------------------------------------------------
 sub prevest
 {
-    my $prevod = shift; # odkaz na hash s převodní tabulkou
+    my $prevod = shift; # reference to the hash with the transliteration table
     my $retezec = shift;
-    my $maxl = shift; # maximální možná délka zdrojové skupiny znaků
-    $maxl = 5 unless($maxl);
+    my $maxl = shift; # maximum possible length of the source substring
+    $maxl = 5 unless($maxl); # default maximum length
     my $vysledek;
     my @chars = split(//, $retezec);
     my $l = scalar(@chars);
@@ -54,48 +55,13 @@ sub prevest
                 $i += $j-1;
                 last;
             }
-            # Pokud se nenašel přepis ani pro samotný aktuální znak, okopírovat tento znak na výstup.
+            # If no transliteration is available for the current character, copy the character to the output.
             elsif($j==1)
             {
                 $vysledek .= $usek;
             }
         }
     }
-    return $vysledek;
-}
-
-
-
-#------------------------------------------------------------------------------
-# Převede řetězec z cizího písma do latinky.
-# Tato stará verze funkce předpokládá, že převodní tabulka obsahuje převody
-# jednotlivých znaků a dvojic znaků, ale ne delších řetězců.
-#------------------------------------------------------------------------------
-sub prevest0
-{
-    my $prevod = shift; # odkaz na hash s převodní tabulkou
-    my $retezec = shift;
-    my @znaky = split(//, $retezec);
-    for(my $i = 0; $i<=$#znaky; $i++)
-    {
-        # Je-li převod dvojice znaků definovaný, použít ho.
-        if($i<$#znaky)
-        {
-            my $src = $znaky[$i].$znaky[$i+1];
-            if(exists($prevod->{$src}))
-            {
-                $znaky[$i] = $prevod->{$src};
-                splice(@znaky, $i+1, 1);
-                $i--;
-                next;
-            }
-        }
-        if(exists($prevod->{$znaky[$i]}))
-        {
-            $znaky[$i] = $prevod->{$znaky[$i]};
-        }
-    }
-    my $vysledek = join("", @znaky);
     return $vysledek;
 }
 
